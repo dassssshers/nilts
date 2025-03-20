f98f9a009776b203cca8eb01fb022523c82ffbd3
Takuma Osada <judgment9ttk069@gmail.com>
2025-03-20 21:29:54 +0900

    fixup! feat: add rule for

--- packages/nilts_test/test/lints/missing_comment_space.dart
+++ packages/nilts_test/test/lints/missing_comment_space.dart
@@ -1,11 +1,13 @@
 // This is a comment
-
-// expect_lint: missing_comment_space
-//This is a comment
-
-/// This is a comment
 class A {}
 
 // expect_lint: missing_comment_space
-///This is a comment
+//This is a comment
 class B {}
+
+/// This is a comment
+class C {}
+
+// expect_lint: missing_comment_space
+///This is a comment
+class D {}
